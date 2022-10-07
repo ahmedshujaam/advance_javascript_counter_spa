d3be3c2baed3c72e5752ec72478fecc026bed09a
ahmedshujaam <ahmedshuja390@gmail.com>
2022-10-08 01:52:26 +0500

    advance javascript spa and counter and username show

---
+++ header.js
@@ -0,0 +1,43 @@
+document.getElementById("header").innerHTML =`
+
+
+<div class="container-fluid">
+    <div class="container">
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">Navbar</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto" >
+                  <li class="nav-item" >
+                    <a class="nav-link active" id="home" aria-current="page" href="#/home">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link " id="about" aria-current="page" href="#/about">about</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link " id="Services" aria-current="page" href="#/service">Services</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link " id="register" aria-current="page" href="#/register">register</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link"> <b><span id="person"></span></b></a>
+                  </li>
+
+                  <li class="nav-item">
+                    <a class="nav-link " id="view" aria-current="page" href="#/counter">user_counts</a>
+                  </li>
+                 
+                  
+                
+                
+              </div>
+            </div>
+          </nav>
+
+
+`
---
+++ spn.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+    $("#home").click(function(){
+        $("#data").load("main.html")
+    })
+
+
+    $("#about").click(function(){
+        $("#data").load("about.html")
+    })
+
+    
+    
+    $("#Services").click(function(){
+        $("#data").load("Services.html")
+    })
+
+ 
+    
+    
+    $("#register").click(function(){
+        $("#data").load("register.html")
+    })
+
+})
